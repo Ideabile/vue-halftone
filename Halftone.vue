516457e0871c8bf2947ea22eb879c0afad8c6364
Mauro Mandracchia <mauro@insided.nl>
2017-05-25 13:48:55 +0200

    Initial Commit

---
+++ Halftone.vue
@@ -0,0 +1,133 @@
+<template>
+  <div class="v-halftone">
+    <svg v-if="img" :viewBox="getViewBox()" preserveAspectRatio="xMinYMid slice" :width="w.width" :height="w.height">
+      <g v-if="web" stroke="#000" fill="none">
+        <g v-for="item, lum in items">
+          <path :stroke-width="lum" stroke-linecap="round" :d="getPathData(item)" />
+        </g>
+      </g>
+      <g v-else stroke="none" fill="#000">
+        <g v-for="item, lum in items">
+          <circle v-for="circle in item" :cx="circle.x * 3" :cy="circle.y * 3" :r="lum * 1.2" />
+        </g>
+      </g>
+    </svg>
+  </div>
+</template>
+<script>
+ function getResizedSizes(w, h, max){
+   let greater = w >= h ? w : h
+   let scale = max / greater
+   if (greater > max) return {
+     height: h * scale,
+     width: w * scale
+   }
+
+   return {
+     height: h,
+     width: w
+   }
+ }
+ export default {
+
+   props: { url: {default: 'http://i.imgur.com/jx3ggSRb.jpg'}, web: {default: true} },
+
+   data(){
+     return {
+       width: 0,
+       height: 0,
+       items: [],
+       img: null,
+       w: {
+         width: window.innerWidth,
+         height: window.innerHeight
+       }
+     }
+   },
+
+   mounted(){
+     if(!this.url) return false
+     this.fetchImage()
+   },
+
+   watch: {
+     url(){
+       this.fetchImage()
+     }
+   },
+
+   methods: {
+
+     fetchImage(){
+       let img = document.createElement('img')
+       img.crossOrigin = "Anonymous";
+       img.onload = ()=>{
+         var canvas = document.createElement('canvas');
+         var context = canvas.getContext('2d');
+         canvas.width = img.width;
+         canvas.height = img.height;
+         context.drawImage(img, 0, 0 );
+         var imgData = context.getImageData(0, 0, img.width, img.height);
+
+         this.img = imgData
+         this.items = this.getBrightnessData( imgData )
+         this.setWindowSizes( window )
+       }
+       img.src = this.url
+     },
+
+     getViewBox(){
+       return `0 0 ${this.img.width} ${this.img.height}`
+     },
+
+     setWindowSizes(w){
+       if(this.width === w.innerWidth || this.height === w.innerHeight) return false;
+       this.width = w.innerWidth
+       this.height = w.innerHeight
+     },
+
+     getPathData(item){
+       let path = [];
+
+       for(let k in item){
+         path.push(`M${item[k].x} ${item[k].y}z`)
+       }
+       return path.join('')
+     },
+
+     getRgba(data, i){
+       return { r: data[i], g: data[i + 1], b: data[i + 2], a: data[i + 3] }
+     },
+
+     getLuminosity(rgba){
+       return (( rgba.a - (rgba.r + rgba.g + rgba.b) / 3) / 200).toFixed(2)
+     },
+
+     isInvisible(rgba){ // Or white
+       return (!rgba.a > 0 || (rgba.r === 255 && rgba.g === 255 && rgba.b === 255))
+     },
+
+     getBrightnessData(img) {
+       let lums = {};
+       let data = img.data
+
+       for(let i = 0; i<data.length; i+=4){
+
+         let rgba = this.getRgba(data, i)
+
+         if ( this.isInvisible(rgba) ) continue
+
+         let lum = this.getLuminosity(rgba);
+         let x = (i / 4) % img.width
+         let y = Math.floor((i / 4) / img.width)
+
+         lums[lum] = lums[lum] || []
+         lums[lum].push({ x, y })
+
+       }
+
+       return lums
+     }
+   }
+ }
+</script>
